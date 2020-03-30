3553c4740925e39fe792c25c3370388bb59f3d69
hcleon4849 <42327254+hcleon4849@users.noreply.github.com>
2020-03-30 22:20:17 +0000

    completed curriculum update form | march 30,2020 @10:19pm.

--- curriculum.php
+++ curriculum.php
@@ -1,5 +1,112 @@
 <!-- page header -->
 <?php include('header.php'); ?>
 
+
+
+    <div class=" outer-body  ">
+        <div class="inner-body d-flex justify-content-center align-items-center">
+            <form class="col-12 col-sm-12 col-md- 9 col-lg-6 col-xl-6 ">
+                <div class="form-header text-center">
+                    <span class="lead">Curriculum Update!</span>
+                </div>
+                <hr>
+                <div class="row ">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                       <div class="row">
+                            <div class="form-group col-12 col-sm-12 col-md- 9 col-lg-6 col-xl-6">
+                                <label><b>Grade:</b></label><br>
+                                <select name="key" class="form-control ">
+                                    <option selected disabled>CHOOSE CLASS</option>
+                                    <option value="first grade"> Grade One</option>
+                                    <option value="second grade">Grade Two</option>
+                                    <option value=" third Grade">Grade Three</option>
+                                    <option value="fourth grade">Grade Four</option>
+                                    <option value="fifth Grade">Grade Five</option>
+                                    <option value="sixth Grade">Grade Six</option>
+                                    <option value="seventh Grade">Grade Seven</option>
+                                    <option value="eighth Grade">Grade Eight</option>
+                                    <option value="nineth Grade">Grade Nine</option>
+                                    <option value="tenth Grade">Grade Ten</option>
+                                    <option value="eleventh Grade">Grade Eleven</option>
+                                    <option value="twelveth Grade">Grade Twelve</option>
+                                    
+                                </select>
+                            </div>
+                            <div class="form-group col-12 col-sm-12 col-md- 9 col-lg-6 col-xl-6">
+                                <label><b>Subject:</b></label>
+                                <select name="key" class="form-control ">
+                                    <option selected disabled>CHOOSE SUBJECT</option>
+                                    <option value="english">English</option>
+                                    <option value="math">Math</option>
+                                    <option value=" social science">Social Science</option>
+                                    <option value=" general knowledge">General Knowledge</option>
+
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                       <div class="row">
+                            <div class="form-group col-12 col-sm-12 col-md- 9 col-lg-6 col-xl-6">
+                                <label><b>Semester:</b></label><br>
+                                <select name="key" class="form-control  ">
+                                    <option selected disabled>CHOOSE SEMESTER</option>
+                                    <option value="first semester">Semester One</option>
+                                    <option value="second semester">Semester Two</option>
+                                </select>
+                            </div>
+                            <div class="form-group col-12 col-sm-12 col-md- 9 col-lg-6 col-xl-6">
+                                <label><b>Period:</b></label><br>
+                                <select name="key" class="form-control ">
+                                    <option selected disabled>CHOOSE PERIOD</option>
+                                    <option value="first period">1st Period</option>
+                                    <option value="second period">2nd Period</option>
+                                    <option value="third period">3rd Period</option>
+                                    <option value="fourth period">4thd Period</option>
+                                    <option value="fifth period">5th Period</option>
+                                    <option value="sixth period">6th Period</option>
+                                </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label><b>Topic:</b></label><br>
+                    <textarea type="text" class="form-control" rows="3" placeholder="provide topic for discussion" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label><b>Learning Outcome:</b></label><br>
+                    <textarea type="text" class="form-control" rows="3" placeholder="knowledge or skills students should acquire by the end of a particular exercise" required></textarea
+                    </div>
+                    <div class="form-group">
+                        <label><b>Objectives:</b></label><br>
+                    <textarea type="text" class="form-control" rows="3"  placeholder="what students will achieved at the end of this exercise" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label><b>Content:</b></label><br>
+                    <textarea type="text" class="form-control" rows="3"  placeholder="what the exercise is about" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label><b>Activities:</b></label><br>
+                    <textarea type="text" class="form-control" rows="3"  placeholder="things students need to do" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label><b>Materials:</b></label><br>
+                    <textarea type="text" class="form-control" rows="3"  placeholder="tools students need" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label><b>Competency Assessment:</b></label><br>
+                    <textarea type="text" class="form-control" rows="3" placeholder="students progress" required></textarea>
+                    </div>
+                    <div class=" mt-2 justify-content-center">
+                        <button type="button" class=" col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12   btn btn-primary btn-md  text-white col-8 justify-self-center mb-3 button" >Save Changes</button>
+                    </div>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
+       
+
+
 <!-- page footer -->
 <?php include('footer.php'); ?>
--- index.php
+++ index.php
@@ -2,242 +2,171 @@
 <?php include('header.php'); ?>
 
 
-
-    <div class="">
-    <!-- class card  -->
-    <div class="row  ">
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3  ">
-            <div class="row  ">
-                <div class="class-card text-center">
-                    <div class="  card-body col-12 col-md-12 col-lg-12 col-xl-12">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-danger d-flex justify-content-center align-items-center">
-                                <div ><i class="fas fa-level-down-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE ONE</h4></span> 
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-danger d-flex justify-content-center align-items-center">
+                        <div >
+                            <h1 class="text-white text-style"><b>1</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE ONE</h4></span> 
                     </div>
                 </div>
-                
-            </div>
-        </div>
-        <!-- class card -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 ">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-primary d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-down-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE TWO</h4></span> 
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-primary d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>2</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE TWO</h4></span> 
                     </div>
                 </div>
-                
-            </div>
-        </div>
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-success d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-down-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE THREE</h4></span> 
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-success d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>3</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE THREE</h4></span> 
                     </div>
                 </div>
-                
-            </div>
-        </div>
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-warning d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-down-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE FOUR</h4></span> 
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-warning d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>4</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE FOUR</h4></span> 
                     </div>
                 </div>
-                
-            </div>
-        </div>
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-danger d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-down-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE FIVE</h4></span> 
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-warning d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>5</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE FIVE</h4></span> 
                     </div>
                 </div>
-                
-            </div>
-        </div>
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-danger d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-down-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE SIX</h4></span> 
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-success d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>6</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE SIX</h4></span> 
                     </div>
                 </div>
-                
-            </div>
-        </div>
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-danger d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>7</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE SEVEN</h4></span> 
+                    </div>
+                </div>
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-primary d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>8</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE EIGHT</h4></span> 
+                    </div>
+                </div>
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-warning d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>9</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE NINE</h4></span> 
+                    </div>
+                </div>
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-primary d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>10</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE TEN</h4></span> 
+                    </div>
+                </div>
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-success d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>11</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE ELEVEN</h4></span> 
+                    </div>
+                </div>
+            </div>    
+            <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <div class="card shadow p-3 mb-5 bg-white rounded border-0 mt-3  d-flex justify-content-center align-items-center ">
+                    <div class="icon-style bg-danger d-flex justify-content-center align-items-center">
+                        <div >
+                        <h1 class="text-white text-style"><b>12</b></h1>
+                        </div>
+                    </div>
+                    <!-- class -->
+                    <div class="card-body">
+                        <span class="card-text"><h4>GRADE TWELVE</h4></span> 
+                    </div>
+                </div>
+            </div>    
         
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-success d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-up-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE SEVEN</h4></span> 
-                    </div>
-                </div>
-                
-            </div>
         </div>
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-warning d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-up-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE EIGHT</h4></span> 
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-danger d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-up-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE NINE</h4></span> 
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-primary d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-up-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE TEN</h4></span> 
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-success d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-up-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE ELEVEN</h4></span> 
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-        <!-- class card  -->
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="row ">
-                <div class="class-card text-center">
-                    <div class="col-12 col-md-12 col-lg-12 col-xl-12  card-body ">
-                        <!-- icon -->
-                        <div class="row d-flex justify-content-center align-items-center p-0 ">
-                            <div class="icon-style bg-warning d-flex justify-content-center align-items-center ">
-                                <div ><i class="fas fa-level-up-alt fa-2x"></i>
-                            </div>
-                            </div>
-                        </div><br>
-                        <!-- class -->
-                    <span><h4>GRADE TWELVE</h4></span> 
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-    
     </div>
+
+
+
     
-    </div>
 
 
 <!-- page footer -->
